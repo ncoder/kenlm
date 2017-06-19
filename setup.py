69c4c16231c69c82dfda3b4fa374315411dc7262
Nicolas Coderre <nicolas.coderre@gmail.com>
2017-06-18 22:34:39 -0700

    ease some dependencies for windows builds. You still need to set BOOST in your INCLUDE and LIB env vars

--- setup.py
+++ setup.py
@@ -13,24 +13,28 @@ def compile_test(header, library):
 FILES = glob.glob('util/*.cc') + glob.glob('lm/*.cc') + glob.glob('util/double-conversion/*.cc')
 FILES = [fn for fn in FILES if not (fn.endswith('main.cc') or fn.endswith('test.cc'))]
 
-LIBS = ['stdc++']
-if platform.system() != 'Darwin':
+LIBS = []
+if platform.system() != 'Windows':
+    LIBS.append('libc++')
+if platform.system() != 'Darwin' and platform.system() != 'Windows':
     LIBS.append('rt')
 
 #We don't need -std=c++11 but python seems to be compiled with it now.  https://github.com/kpu/kenlm/issues/86
 ARGS = ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER=6', '-std=c++11']
 
-if compile_test('zlib.h', 'z'):
-    ARGS.append('-DHAVE_ZLIB')
-    LIBS.append('z')
+#these dependencies take some extra work to get going in windows. punt for now.
+if platform.system() != 'Windows':
+    if compile_test('zlib.h', 'z'):
+        ARGS.append('-DHAVE_ZLIB')
+        LIBS.append('z')
 
-if compile_test('bzlib.h', 'bz2'):
-    ARGS.append('-DHAVE_BZLIB')
-    LIBS.append('bz2')
+    if compile_test('bzlib.h', 'bz2'):
+        ARGS.append('-DHAVE_BZLIB')
+        LIBS.append('bz2')
 
-if compile_test('lzma.h', 'lzma'):
-    ARGS.append('-DHAVE_XZLIB')
-    LIBS.append('lzma')
+    if compile_test('lzma.h', 'lzma'):
+        ARGS.append('-DHAVE_XZLIB')
+        LIBS.append('lzma')
 
 ext_modules = [
     Extension(name='kenlm',
